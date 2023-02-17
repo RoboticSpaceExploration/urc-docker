988dd6f73ca4a8c9ea8aa5cbf69c2a9d891df104
Bret Witt <bretwitt@hawaii.edu>
2023-02-17 19:55:53 +0000

    Fix static ip

--- rpi/rpi-ros-perception/run.sh
+++ rpi/rpi-ros-perception/run.sh
@@ -1,6 +1,7 @@
 sudo docker run -it --rm \
     --privileged \
     --net=host \
+    -e ROS_IP=128.171.151.2 \
     -v /dev:/dev \
     --device-cgroup-rule "c 81:* rmw" \
     --device-cgroup-rule "c 189:* rmw" \
