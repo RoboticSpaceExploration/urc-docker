895910908bcd5e8bad90343e0db2d6a4bdf8c30d
Bret Witt <bretwitt@hawaii.edu>
2023-02-17 19:45:14 +0000

    update image

--- rpi/rpi-ros-perception/run.sh
+++ rpi/rpi-ros-perception/run.sh
@@ -1,1 +1,7 @@
-sudo docker run --rm -it rpi-ros-perception  
+sudo docker run -it --rm \
+    --privileged \
+    --net=host \
+    -v /dev:/dev \
+    --device-cgroup-rule "c 81:* rmw" \
+    --device-cgroup-rule "c 189:* rmw" \
+    bretwitt/rpi-ros-perception /bin/bash
