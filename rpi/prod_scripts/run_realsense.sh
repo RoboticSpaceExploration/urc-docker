a2419ae05b2508ca531ac9a4a429b2f6c1950ded
Bret Witt <bretwitt@hawaii.edu>
2023-02-25 17:34:13 -1000

    Small cleanup

--- rpi/prod_scripts/run_realsense.sh
+++ rpi/prod_scripts/run_realsense.sh
@@ -1,8 +1,10 @@
-sudo docker run -it --rm \
+sudo docker run --rm \
     --privileged \
     --net=host \
     -e ROS_IP="$(cat prod_ip)" \
+    -e ROS_MASTER_URI="192.168.2.1" \
     -v /dev:/dev \
     --device-cgroup-rule "c 81:* rmw" \
     --device-cgroup-rule "c 189:* rmw" \
-    bretwitt/rpi-ros-perception /bin/bash
+    --detached
+    bretwitt/rpi-ros-perception 
--- rpi/rpi-ros-perception/run.sh
+++
@@ -1,8 +0,0 @@
-sudo docker run -it --rm \
-    --privileged \
-    --net=host \
-    -e ROS_IP=128.171.151.2 \
-    -v /dev:/dev \
-    --device-cgroup-rule "c 81:* rmw" \
-    --device-cgroup-rule "c 189:* rmw" \
-    bretwitt/rpi-ros-perception /bin/bash
